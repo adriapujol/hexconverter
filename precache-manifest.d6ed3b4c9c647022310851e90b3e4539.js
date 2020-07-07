5e9d0e9e843ba79a758a69fa660f0b87d3f3c4d8
Adria Pujol <adriapujolaparicio@gmail.com>
2020-07-07 17:49:17 +0200

    Updates

---
+++ precache-manifest.d6ed3b4c9c647022310851e90b3e4539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfac2227299ced9668054a5d2b3bb0e",
+    "url": "/adriapujol/hexconverter.git/index.html"
+  },
+  {
+    "revision": "d9241172255e3ded9a40",
+    "url": "/adriapujol/hexconverter.git/static/css/main.d2d5e64b.chunk.css"
+  },
+  {
+    "revision": "7d2c14c8e4ae91fc0ec4",
+    "url": "/adriapujol/hexconverter.git/static/js/2.dde17fb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adriapujol/hexconverter.git/static/js/2.dde17fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9241172255e3ded9a40",
+    "url": "/adriapujol/hexconverter.git/static/js/main.0cbd960f.chunk.js"
+  },
+  {
+    "revision": "6a1def6026a314607a4b",
+    "url": "/adriapujol/hexconverter.git/static/js/runtime-main.7671f2e5.js"
+  }
+]);
