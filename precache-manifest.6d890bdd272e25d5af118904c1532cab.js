ce04aa6dc226c7569f3fc30ed2228b149b6098d3
Adria Pujol <adriapujolaparicio@gmail.com>
2020-07-07 18:37:54 +0200

    Updates

---
+++ precache-manifest.6d890bdd272e25d5af118904c1532cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d449697981ace92f880034da992c82f3",
+    "url": "/adriapujol/hexconverter/index.html"
+  },
+  {
+    "revision": "4d5d17cc316aa5a40a6a",
+    "url": "/adriapujol/hexconverter/static/css/main.97345299.chunk.css"
+  },
+  {
+    "revision": "7d2c14c8e4ae91fc0ec4",
+    "url": "/adriapujol/hexconverter/static/js/2.dde17fb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adriapujol/hexconverter/static/js/2.dde17fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5d17cc316aa5a40a6a",
+    "url": "/adriapujol/hexconverter/static/js/main.bccfbd1a.chunk.js"
+  },
+  {
+    "revision": "8fc29747b61da0d85093",
+    "url": "/adriapujol/hexconverter/static/js/runtime-main.2c8c848b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adriapujol/hexconverter.git/precache-manifest.de8c4f061ac5771088ca5273c712d30d.js"
+  "/adriapujol/hexconverter/precache-manifest.6d890bdd272e25d5af118904c1532cab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adriapujol/hexconverter.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adriapujol/hexconverter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
